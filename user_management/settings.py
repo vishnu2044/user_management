302f49cdb5b52d6a88bae60fd2210e7ba74cb692
vishnu narayanan <vishnunarayanan2044@gmail.com>
2024-01-15 10:56:44 +0530

    data base changed into sqlite3

--- user_management/settings.py
+++ user_management/settings.py
@@ -78,17 +78,23 @@ WSGI_APPLICATION = 'user_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'user_management',
+#         'USER': 'postgres',
+#         'PASSWORD': 'vishnu2044',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'user_management',
-        'USER': 'postgres',
-        'PASSWORD': 'vishnu2044',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
